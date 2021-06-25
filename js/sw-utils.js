aa410e3168eafcdfde49a6359e565387f171f43b
Danilo <danilovidalm@gmail.com>
2021-06-25 15:29:46 -0400

    first commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+// Guardar en el cache dinamico
+function actualizaCacheDinamico(dynamicCache, req, resp) {
+    if (resp.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, resp.clone());
+            return resp.clone;
+        })
+    } else {
+        return resp;
+    }
+}
