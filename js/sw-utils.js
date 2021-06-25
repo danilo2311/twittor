54485b08481fd386fb15a37f3abad6c6b2ff598e
Danilo <danilovidalm@gmail.com>
2021-06-25 15:40:23 -0400

    corregido problema en sw-utils.js

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -3,7 +3,7 @@ function actualizaCacheDinamico(dynamicCache, req, resp) {
     if (resp.ok) {
         return caches.open(dynamicCache).then(cache => {
             cache.put(req, resp.clone());
-            return resp.clone;
+            return resp.clone();
         })
     } else {
         return resp;
