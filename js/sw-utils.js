d0fdb1368d851dde71f0014e7d6e037a60162534
Danilo <danilovidalm@gmail.com>
2021-06-26 23:12:26 -0400

    elimino clg en  function file sw-utils.js

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -2,7 +2,6 @@
 function actualizaCacheDinamico(dynamicCache, req, resp) {
     if (resp.ok && !resp.url.includes('chrome-extension')) {
         return caches.open(dynamicCache).then(cache => {
-            console.log(cache);
             cache.put(req, resp.clone());
             return resp.clone();
         })
